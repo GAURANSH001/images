56be3c03b09f5e2a109996f8d90d63e145fb1312
GAURANSH001 <gauransh.sharma0602@gmail.com>
2019-07-15 12:05:51 +0530

    sample is ready

--- src/App.js
+++ src/App.js
@@ -3,8 +3,13 @@ import logo from './logo.svg';
 import './App.css';
 import axios from 'axios';
 import { Button, List,Carousel} from 'antd';
-import 'antd/dist/antd.css'
+import 'antd/dist/antd.css';
+import { Menu, Icon } from 'antd';
+import { Input } from 'antd';
 
+const { Search } = Input;
+
+const { SubMenu } = Menu;
 
  
  
@@ -52,15 +57,83 @@ getvalue(e){
 }
 
   render(){
-    return <div>
+    return <div className="container-fluid">
+      <div className="color">
+      <Menu onClick={this.handleClick} selectedKeys={[this.state.current]} mode="horizontal">
+        <Menu.Item key="logo">
+          
+         <a href="https://www.cleartrip.com/trains/list" target="_blank">HOME</a>
+        </Menu.Item>
+    
+        <SubMenu
+          title={
+            <span className="submenu-title-wrapper">
+              <Icon type="" />
+               ABOUT
+            </span>
+          }
+        >
+          <Menu.ItemGroup title="">
+            <Menu.Item key="setting:1"> <a href="https://www.orientrailjourneys.com/blog/history-of-indian-railways/">HISTORY</a></Menu.Item>
+            
+          </Menu.ItemGroup>
+          <Menu.ItemGroup title="">
+            <Menu.Item key="setting:3"><a href="https://swarajyamag.com/infrastructure/nine-rail-projects-in-india-to-look-out-for" target="_blank">UPCOMING PROJECTS</a></Menu.Item>
+          </Menu.ItemGroup>
+          
+        </SubMenu>
+        <SubMenu
+          title={
+            <span className="submenu-title-wrapper">
+              <Icon type="" />
+               CONTACTS
+            </span>
+          }
+        >
+          <Menu.ItemGroup title="">
+            <Menu.Item key="setting:1">8949484623 </Menu.Item>
+            
+          </Menu.ItemGroup>
+          <Menu.ItemGroup title="">
+            <Menu.Item key="setting:3">9461285485</Menu.Item>
+          </Menu.ItemGroup>
+          
+        </SubMenu>
+        <SubMenu
+          title={
+            <span className="submenu-title-wrapper">
+              <Icon type="" />
+              <Search placeholder="input search text" onSearch={value => console.log(value)} enterButton />
+               
+            </span>
+          }
+        >
+          
+         
+          
+        </SubMenu>
+        <Menu.Item key="alipay">
+          <a href="https://ant.design" target="_blank" rel="noopener noreferrer">
+            
+          </a>
+        </Menu.Item>
+      </Menu>
+      </div>
        <Carousel autoplay>
     <div>
-      <img width="1012px" height="300px" src="https://images.pexels.com/photos/433301/pexels-photo-433301.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500" alt="sorry"></img>
+      <img width="1012px" height="400px" src="https://github.com/GAURANSH001/images/blob/master/IMG6.jpg?raw=true" className="de" alt="sorry"></img>
     </div>
     <div>
       
-      <img width="1012px" height="300px" src="https://images7.alphacoders.com/661/thumb-1920-661783.jpg"></img>
+      <img width="1012px" height="400px" src="https://github.com/GAURANSH001/images/blob/master/img3.jpg?raw=true"></img>
     </div>
+    <div>
+    <img width="1012px" height="400px" src="https://ak1.picdn.net/shutterstock/videos/26887621/thumb/1.jpg"></img>
+    </div>
+    <div>
+    <img width="1012px" height="400px" src="https://ak4.picdn.net/shutterstock/videos/15040414/thumb/11.jpg"></img>
+    </div>
+    
     
   </Carousel>   
            
